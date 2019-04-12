1240208fdf1c99f0744f4780145724de0bda48c6
Renan Pupin <renan.pupin@gmail.com>
2019-04-12 17:31:31 -0300

    work on maps integration

--- public/app.js
+++ public/app.js
@@ -16,15 +16,33 @@ async function getPoints(){
             throw new Error(responseBody.message);
         }else{
 
+            let size = 503.947;
+            let scale = 50;
             responseBody.points.forEach((item) => {
                 let myLatlng = new google.maps.LatLng(item.location.coordinates[1], item.location.coordinates[0]);
 
                 let marker = new google.maps.Marker({
                     position: myLatlng,
                     map: map,
-                    title: item.name
+                    title: item.name,
+                    icon: {
+                        url: './rocket.svg',
+                        size: new google.maps.Size(size, size),
+                        scaledSize: new google.maps.Size(scale, scale),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(scale/2, scale)
+                    },
+                    animation: google.maps.Animation.BOUNCE,
                 });
             });
+
+            // function toggleBounce() {
+            //     if (marker.getAnimation() !== null) {
+            //         marker.setAnimation(null);
+            //     } else {
+            //         marker.setAnimation(google.maps.Animation.BOUNCE);
+            //     }
+            // }
         }
     } catch (err) {
         console.log(err);
@@ -83,44 +101,96 @@ function initMap() {
         zoom: 2
     });
 
-    google.maps.event.addListener(map, 'mousemove', function (event) {
-        console.log(event.latLng.lat(), event.latLng.lng());
-    });
+    //https://developers.google.com/maps/documentation/javascript/examples/directions-draggable
+
+    // google.maps.event.addListener(map, 'mousemove', function (event) {
+        // console.log(event.latLng.lat(), event.latLng.lng());
+    // });
+
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            // console.log(pos);
+
+            let size = 512;
+            let scale = 75;
+            let userLocationMarker = new google.maps.Marker({
+                position: pos,
+                icon: {
+                    url: './astronaut.svg',
+                    size: new google.maps.Size(size, size),
+                    scaledSize: new google.maps.Size(scale, scale),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(scale/2, scale)
+                },
+                map: map,
+                title: "Sua localização",
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                // animation: google.maps.Animation.BOUNCE,
+            });
+
+            map.setCenter(pos);
+            map.setZoom(6);
+
+            var userLocationRadius = new google.maps.Circle({
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.5,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.15,
+                map: map,
+                center: userLocationMarker.position,
+                radius: 200000, //in meters
+                editable: true
+            });
+
+            userLocationMarker.addListener('drag', function (event) {
+                console.log("drag", event.latLng.lat(), event.latLng.lng());
+
+                userLocationRadius.setCenter({
+                    lat: event.latLng.lat(),
+                    lng: event.latLng.lng()
+                });
+            });
+
+            userLocationMarker.addListener('dragend', function (event) {
+                console.log("dragend", event.latLng.lat(), event.latLng.lng());
+
+                map.panTo(event.latLng);
+            });
+
+        }, function(err) {
+            console.log(err);
+            alert("Erro: "+err.message);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        alert("Seu browser não suporta Geolocation.");
+    }
 
     getPoints();
     getPolygon();
 
-    // $.get( "http://localhost:1345/", function( data ) {
+
+    // google.maps.event.addListener(circle, 'radius_changed', function() {
+    //     console.log(circle.getRadius());
+    // });
     //
-    //     var lat=-20.1;
-    //     var lon=50.1;
+    // google.maps.event.addListener(outerPath, 'set_at', function() {
+    //     console.log('Vertex moved on outer path.');
+    // });
     //
-    //     var myLatlng = new google.maps.LatLng(lat, lon);
-    //     var mapProp = {
-    //         center:myLatlng,
-    //         zoom:5,
-    //         mapTypeId:google.maps.MapTypeId.ROADMAP,
-    //         mapTypeControl: false
-    //     };
+    // google.maps.event.addListener(innerPath, 'insert_at', function() {
+    //     console.log('Vertex removed from inner path.');
+    // });
     //
-    //     //criando objeto mapa
-    //     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    //
-    //     var obj = jQuery.parseJSON(data);
-    //
-    //     $.each($.parseJSON(data), function(idx, obj) {
-    //
-    //         var lat=obj.lat;
-    //         var lon=obj.lon;
-    //
-    //         var myLatlng = new google.maps.LatLng(lat, lon);
-    //
-    //         var marker = new google.maps.Marker({
-    //             position: myLatlng,
-    //             map: map,
-    //             title: obj.nome
-    //         });
-    //     });
-    //
+    // google.maps.event.addListener(rectangle, 'bounds_changed', function() {
+    //     console.log('Bounds changed.');
     // });
 }
